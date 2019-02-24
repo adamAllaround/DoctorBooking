c7eeb486b9ac2048f1c9e7e9f99f768df2b84a0b
adamAllaround <adam@allaroundjava.com>
2019-02-24 19:38:33 +0000

    Removing unnecessary bi-directional mapping from Doctor and Patient

--- DoctorBookingApplication/src/main/java/com/allaroundjava/model/Patient.java
+++ DoctorBookingApplication/src/main/java/com/allaroundjava/model/Patient.java
@@ -3,16 +3,11 @@ package com.allaroundjava.model;
 import org.hibernate.annotations.Immutable;
 
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import java.util.Set;
 
 @Entity
 @Immutable
 public class Patient extends ModelBase {
     private String name;
-
-    @OneToMany(mappedBy = "patient")
-    private Set<Appointment> appointments;
 
     Patient() {
     }
@@ -23,9 +18,5 @@ public class Patient extends ModelBase {
 
     public String getName() {
         return name;
-    }
-
-    public Set<Appointment> getAppointments() {
-        return appointments;
     }
 }
