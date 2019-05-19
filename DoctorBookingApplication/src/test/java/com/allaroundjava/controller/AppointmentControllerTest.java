bc40a5fef3afc635bc5042a0a598d685a7e9cf43
Adam Stankowski <adam@allaroundjava.com>
2019-05-19 21:23:36 +0200

    Adding test method to AppointmentController

--- DoctorBookingApplication/src/test/java/com/allaroundjava/controller/AppointmentControllerTest.java
+++ DoctorBookingApplication/src/test/java/com/allaroundjava/controller/AppointmentControllerTest.java
@@ -1,8 +1,8 @@
 package com.allaroundjava.controller;
 
+import com.allaroundjava.model.Patient;
 import com.allaroundjava.service.AppointmentService;
 import com.allaroundjava.service.AppointmentSlotService;
-import com.allaroundjava.service.DoctorService;
 import com.allaroundjava.service.PatientService;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,6 +21,7 @@ public class AppointmentControllerTest {
     private AppointmentSlotService appointmentSlotService;
     private PatientService patientService;
     private MockMvc mockMvc;
+    private final static String REQUEST_BODY = "<appointmentDto><doctorId>1</doctorId><patientId>1</patientId><startTime>2019-05-13T20:00:00</startTime><endTime>2019-05-13T21:00:00</endTime></appointmentDto>";
 
     public AppointmentControllerTest() {
         this.appointmentService = Mockito.mock(AppointmentService.class);
@@ -37,10 +38,20 @@ public class AppointmentControllerTest {
     @Test
     public void whenBookingAppointment_andPatientNotExists_thenNotFound() throws Exception {
         Mockito.doReturn(Optional.empty()).when(patientService).getById(1L);
-        String requestBody="<appointmentDto><doctorId>1</doctorId><patientId>1</patientId><startTime>2019-05-13T20:00:00</startTime><endTime>2019-05-13T21:00:00</endTime></appointmentDto>";
         mockMvc.perform(MockMvcRequestBuilders.post("/appointments")
                 .contentType(MediaType.APPLICATION_XML)
-                .content(requestBody))
+                .content(REQUEST_BODY))
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError());
+    }
+
+    @Test
+    public void whenBookingAppointment_andAppointmentSlotNotExists_thenNotFound() throws Exception {
+        Patient patient = new Patient("Patient John");
+        Mockito.doReturn(Optional.of(patient)).when(patientService).getById(1L);
+        Mockito.doReturn(Optional.empty()).when(appointmentSlotService).getById(1L);
+        mockMvc.perform(MockMvcRequestBuilders.post("/appointments")
+                .contentType(MediaType.APPLICATION_XML)
+                .content(REQUEST_BODY))
                 .andExpect(MockMvcResultMatchers.status().is4xxClientError());
     }
 }
