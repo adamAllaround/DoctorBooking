4cc5c661e6fc1d10b164b8505b5f93bceb71664c
adamAllaround <adam@allaroundjava.com>
2019-09-08 08:47:40 +0200

    Adding AppointmentSlotController Api in swagger

--- DoctorBookingApplication/src/main/java/com/allaroundjava/controller/AppointmentSlotController.java
+++ DoctorBookingApplication/src/main/java/com/allaroundjava/controller/AppointmentSlotController.java
@@ -23,7 +23,7 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/slots")
-public class AppointmentSlotController {
+public class AppointmentSlotController implements SlotsApi {
     private final AppointmentSlotService appointmentSlotService;
     private final DoctorService doctorService;
 
